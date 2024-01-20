c921e8d89fa923fdd1ba7c4defd4adb2137be0b0
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-01-20 15:31:31 +0300

    fiber, 2 new handlers

--- main.go
+++ main.go
@@ -3,22 +3,56 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 const (
 	port = ":3000"
 )
 
-func itemsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Request received", r.Method)
-	w.Write([]byte("hello from server"))
+type Item struct {
+	Id int `json:"id"`
+	Title string `json:"title"`
+	Price float32 `json:"price"`
+}
+
+var items []Item = []Item{
+	{Id: 0, Title: "First item", Price: 10.2},
+	{Id: 1, Title: "Second item", Price: 5.1},
+	{Id: 2, Title: "Third item", Price: 4.5},
+	{Id: 3, Title: "Fourth item", Price: 8.0},
+	{Id: 4, Title: "Fifth item", Price: 0.5},
+}
+
+func getItems(c *fiber.Ctx) error {
+	fmt.Println(c.Method(), c.Path())
+	return c.JSON(items)
+}
+
+func getSingleItem(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	
+	if err != nil {
+		return c.Status(http.StatusBadRequest).SendString("Incorrect id")
+	}
+
+	for _, item := range items {
+		if item.Id == id {
+			return c.JSON(item)
+		}
+	}
+
+	return c.Status(http.StatusNotFound).SendString("Item not found")
 }
 
 func main() {
-	http.HandleFunc("/", itemsHandler)
-	fmt.Printf("server started on PORT%s\n", port)
+	app := fiber.New()
 
-	err := http.ListenAndServe(port, nil)
+	app.Get("/items", getItems)
+	app.Get("/items/:id", getSingleItem)
+
+	err := app.Listen(port)
 	if err != nil {
 		fmt.Println("server start error", err)
 	}
